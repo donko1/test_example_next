2bccb3b224041a82c65fa6903ede7dd23dc38a20
donko1 <zalbertic@mail.ru>
2025-04-01 16:47:26 +0300

    Adding strings and arrays tests

--- __tests__/example.test.js
+++ __tests__/example.test.js
@@ -36,3 +36,26 @@ test('adding floating point numbers', () => {
   expect(value).toBeCloseTo(0.3); // This works.
 });
 
+test('there is no I in team', () => {
+  expect('team').not.toMatch(/I/);
+});
+
+test('but there is a "stop" in Christoph', () => {
+  expect('Christoph').toMatch(/stop/);
+});
+
+const shoppingList = [
+  'diapers',
+  'kleenex',
+  'trash bags',
+  'paper towels',
+  'milk',
+];
+
+test('the shopping list has milk on it', () => {
+  expect(shoppingList).toContain('milk');
+  expect(new Set(shoppingList)).toContain('milk');
+});
+
+
+
